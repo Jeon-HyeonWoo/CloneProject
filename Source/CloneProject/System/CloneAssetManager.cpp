4344cfdd3a56832536f063e8d0dce65e46123a5c
jhw096 <jhw096@gmail.com>
2025-02-11 23:35:38 +0900

    250211_AssetManager_SyncLoading, AssetLoading

---
+++ Source/CloneProject/System/CloneAssetManager.cpp
@@ -0,0 +1,114 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CloneAssetManager.h"
+#include "CloneProject/CloneLogChannels.h"
+
+
+
+UCloneAssetManager::UCloneAssetManager()
+{
+}
+
+UCloneAssetManager& UCloneAssetManager::Get()
+{
+	check(GEngine);
+
+	if (UCloneAssetManager* Singleton = Cast<UCloneAssetManager>(GEngine->AssetManager))
+	{
+		return *Singleton;
+	}
+
+	UE_LOG(LogClone, Fatal, TEXT("invalid AssetManagerClassName in DefaultEngine.ini(project settings); it must be CloneAssetManager"));
+
+	return *NewObject<UCloneAssetManager>();
+}
+
+/*
+	현재 빌드 방법이 Development Editor인데,
+	디버깅에 관한 것들을 최소화 하는 대신, 속도가 빨라진다.
+	해당 매크로 사용시 이 부분을 제외하고 Development로 빌드한다.
+	따라서 필요에 따라 이 부분은 빌드를 꼭 하고 가야한다는 뜻.
+*/
+PRAGMA_DISABLE_OPTIMIZATION
+void UCloneAssetManager::StartInitialLoading()
+{
+	//기본적으로 Unreal은 Multi Thead 코딩을 지원한다.
+	//단 Editor가 켜지기 전 까지는 Multi Thread상황에 safe 하지 않는다.
+	Super::StartInitialLoading();
+}
+PRAGMA_ENABLE_OPTIMIZATION
+
+
+bool UCloneAssetManager::ShouldLogAssetLoads()
+{
+	/*
+		FCommandLine : Cmd에 실행한 명령줄에 대한 char* 값을 Get.
+		Param에 CommandLine에 TEXT값을 비교 
+	*/
+	/*
+		Debug Arguments에 -LoadAssetLoads를 추가하여 해당 Args를 사용시 동기, 비동기 식으로 로드 방법을 구분한다.
+	*/
+	const TCHAR* CommandLineContent = FCommandLine::Get();
+	static bool bLogAssetLoads = FParse::Param(CommandLineContent, TEXT("LogAssetLoads"));
+	return bLogAssetLoads;
+}
+
+/*
+	1. 동기식 Load
+	Task_A 와 Task_B가 있다고 가정.
+	Task_A의 Load가 완료되기 전까진, Task_B로 넘어갈 수 없다. 
+	- 장점 : 속도가 빠르다. 로직 순서 캐치가 쉽다.
+	- 단점 : 게임같이 다 클라이언트가 공용해서 사용할 경우 불편함을 겪을 수 있다.
+
+	2. 비동기식 Load
+	Task_A의 Load가 오래걸려 Task_B의 기능을 당장 쓰지 못할 경우.
+	Thread를 통해 Task_A의 Load를 잡아두고, Task_B로 넘어가게 한다.
+	- 게임의 경우는 비동기식 Loading을 쓰는 경우가 많다.
+
+	그렇다면 이 함수의 존재 의의는 동기식을 사용하는 client Load등 동기식으로 진행이 필요할 경우를 위해서이다.
+	* AssetManager에 이미 존재하는 함수지만, 왜 따로 구현하였는가? -> 시간초를 재기 위함이다.
+	* 동기 로드가운데 오래 걸리는 로딩이 있다면, 동기 로드의 방식을 바꿀 필요가 있기 때문에 시간초를 설정한다.
+*/
+/*
+	Asset_A, Asset_B, Asset_C가 있다고 가정. 해당 Asset을 Load하는 과정.
+	각각의 Asset은 각각의 경로가 존재하고. 
+	FSoftObjectPath는 그 경로의 값이다.
+*/
+UObject* UCloneAssetManager::SynchronousLoadAsset(const FSoftObjectPath& AssetPath)
+{
+	if (AssetPath.IsValid())
+	{
+		TUniquePtr<FScopeLogTime> LogtimePtr;
+
+		if (ShouldLogAssetLoads())
+		{
+			//동기로드가 실행되면 몇 초가 걸리는지에 대한 Check.
+			LogtimePtr = MakeUnique<FScopeLogTime>(*FString::Printf(TEXT("synchronous loaded assets [%s]"), *AssetPath.ToString()), nullptr, FScopeLogTime::ScopeLog_Seconds);
+		}
+
+		if (UAssetManager::IsValid())
+		{
+			//GetStreamableManager : 실제로 동기 비동기로드를 돕는 Manager.
+			//아래의 경우는 Synchrounus를 사용하니 AssetPath를 실제로 Synchronous로 실행한다.
+			return UAssetManager::GetStreamableManager().LoadSynchronous(AssetPath);
+		}
+		//AssetManager가 Engine에서 만들어 지지 않을 경우를 대비.
+		//TryLoad는 주석에 매우 느리다고 되어있다. 그래서 비교적 사용하지 않는 방법을 추천.
+		//TryLoad코드에 항상 StaticLoadObject를 하기전에 StaticFindObject를 하는데, 이는 메모리에 캐싱이 되어있는지 체크를 해준다.
+		return AssetPath.TryLoad();
+	}
+
+	return nullptr;
+	
+}
+
+void UCloneAssetManager::AddLoadedAsset(const UObject* Asset)
+{
+	if (ensureAlways(Asset))
+	{
+		//변수 FCriticalSection은 일종의 Safe로, LoadedAssets에 한 개씩 저장이 되도록 Lock을 걸어주는 것이다.
+		//AssetManager는 Multi Thread 환경에 safe하지 않음으로, Editor가 열리기 전에 생성되는 AssetManager에 대해 안전장치를 걸어둔 것.
+		FScopeLock Lock(&SyncObject);
+		LoadedAssets.Add(Asset);
+	}
+}
---
+++ Source/CloneProject/System/CloneAssetManager.h
@@ -0,0 +1,144 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "CloneAssetManager.generated.h"
+
+
+/*
+	1. 사용 이유 : AssetManager는 PrimaryAsset에 대한 Load, unload를 해준다.
+	2. 목적 : AssetManager를 사용하는 이유는 static ConstructorHelper를 이용한 objectfind를 사용시 하드코딩을 피하게 해준다.
+		- 100개의 Asset에 대한 경로와 설정을 일일이 해줄 것인가?
+		- 경로 변경시 모든 에셋에 대한 경로를 일일이 체크할 것인가?
+*/
+
+/**
+	SingleTon은 유일성을 보장해야 하는 디자인 패턴.
+
+	AssetManager는 GEngine에 캐싱이 되어있어 우리가 만든 CustomAssetManager에 Casting한 이후 반환한다.
+
+	return을 하는 이유는 Return 형식을 맞추기 위함.
+**/
+
+UCLASS()
+class CLONEPROJECT_API UCloneAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+
+	UCloneAssetManager();
+
+	//SingleTon에 가장 기본적인 형태로. 단 하나의 Instance를 가져오기 위해 Get을 만든다
+	static UCloneAssetManager& Get();
+
+	virtual void StartInitialLoading() final;
+
+	/*
+		해당 함수는 FScopelogTime을 사용할지 않을지 유무 판단을 위해 사용.
+	*/
+	static bool ShouldLogAssetLoads();
+
+	static UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+
+	//template의 함수는 컴파일 단계에서 코드로 만들어지기 때문에 함수의 정의를 .h에서 정의한다.
+	/*
+		GetAsset은 정적 로딩으로 BP Class와 Object를 로딩
+		- bKeepInMemory 주목
+	*/
+	template <typename AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	template <typename AssetType>
+	static TSubclassOf<AssetType> GetSubclass(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	// [Thread-safe] 메모리에 로딩된 에셋 캐싱
+	void AddLoadedAsset(const UObject* Asset);
+
+	/*
+		Asset들은 Unreal 자체의 Garbage Collection(.GC)에서 관리하는데, 해당 Set에 LoadedAsset을 넣어
+		GC에서 삭제처리를 하지 않도록 함.
+		즉, 캐싱 용도로 사용.
+	*/
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+
+	//Object 단위의 Locking
+	FCriticalSection SyncObject;
+};
+
+/*
+	* Asset Type 차이
+	Asset : Blueprint, Sound	// 완성된 객체
+	TSubclass : 특정 클래스의 Subclass의 타입정보
+	CDO : 클래스의 기본 인스턴스
+	staticclass : MetaData (리플렉션)				
+	
+	* 리플렉션이란? : 런타임 단계에 '클래스에 대한 정보'를 알 수 있도록 하는 것.
+					클래스 내부의 변수, 함수 이름 등등 알 수 있고, 리플렉션을 통해 내부 변수나 함수를 바꿀 수 있다.
+	* Unreal에서는 UPROPERTY를 통해 Detail에서 변수를 수정할 수 있다.
+		이 변수들의 이름이나 변경을 할 수 있도록 하는 것이 리플렉션이다.
+
+	* Unreal Engine의 Build파일이 C#으로 된 이유는 C++에는 리플렉션 기능이 없기 때문이다.
+	* Unreal Engine의 UPROPERTY는 리플렉션 기능을 제공하기 위함이고, Generated_Body, ClassName.generated.h 파일이 이 때문이다.
+	* generated는 주석에 절대 수정하면 안된다는 문구가 존재한다.
+	* 
+	* MyClass::StaticClass()를 살펴보면, UClass를 반환하는 것을 알 수 있다. 
+*/
+
+template<typename AssetType>
+AssetType* UCloneAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+	if (AssetPath.IsValid())
+	{
+		//Loading이 되어있다? -> 바로 가져온다.
+		//Loading이 안 되어있다? -> Null반환
+		LoadedAsset = AssetPointer.Get();
+		if (!LoadedAsset)
+		{
+			LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load Asset [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedAsset && bKeepInMemory)
+		{
+			//LoadedAssets.Add(LoadedAsset); 을 해도 되는데 왜 한번 랩핑을 해놨는가?
+			//AddLoadedAsset을 보면 Lock을 한 번 해놨다.
+			//Lock의 이유는 한 번 등록되면 직접 내리지 않는 한 UnLoad가 되지 않음, 즉 캐싱을 하는 것.
+			Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+		}
+	}
+
+	return LoadedAsset;
+}
+/*
+	TSubclass도 Staticclass처럼 메타 데이터(UClass)를 저장할 수 있다.
+	TSubclass<T> 는 T Type을 상속받은 개체들만 받을 수 있는 안전장치 같은 것.
+*/
+template<typename AssetType>
+TSubclassOf<AssetType> UCloneAssetManager::GetSubclass(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load Asset [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+
+	return LoadedSubclass;
+}
